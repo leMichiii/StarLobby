215788ffcb26fbcf079cee26697f8661a0f9b7ac
Michael Mawick <michaeljens09@gmail.com>
2025-05-05 20:49:47 +0200

    Added whole plugin structure

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "de.michaelmawick"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+}
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/de/michaelmawick/starlobby/StarLobby.java
@@ -0,0 +1,4 @@
+package de.michaelmawick.starlobby;
+
+public class StarLobby {
+}
