4548c1bc58ec6d03344d97fde3011e83f67a1494
Michael Mawick <michaeljens09@gmail.com>
2025-05-06 17:05:46 +0200

    added 2 items for plugin

--- src/main/java/de/michaelmawick/starlobby/listener/ConnectionListener.java
+++ src/main/java/de/michaelmawick/starlobby/listener/ConnectionListener.java
@@ -2,12 +2,15 @@ package de.michaelmawick.starlobby.listener;
 
 import de.michaelmawick.starlobby.StarLobby;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ConnectionListener implements Listener {
 
@@ -22,6 +25,19 @@ public class ConnectionListener implements Listener {
         player.setFoodLevel(StarLobby.getInstance().getConfig().getInt("food-level"));
         player.setMaxHealth(StarLobby.getInstance().getConfig().getInt("max-health"));
         player.setHealth(StarLobby.getInstance().getConfig().getInt("health"));
+
+        ItemStack warps = new ItemStack(Material.valueOf(String.valueOf(StarLobby.getInstance().getConfig().get("warps.item"))));
+        ItemMeta warpsMeta = warps.getItemMeta();
+        warpsMeta.setDisplayName(StarLobby.getInstance().getConfig().getString("warps.displayname"));
+        warps.setItemMeta(warpsMeta);
+
+        ItemStack infobook = new ItemStack(Material.valueOf(String.valueOf(StarLobby.getInstance().getConfig().get("infos.item"))));
+        ItemMeta infobookMeta = infobook.getItemMeta();
+        infobookMeta.setDisplayName(StarLobby.getInstance().getConfig().getString("infos.displayname"));
+        infobook.setItemMeta(infobookMeta);
+
+        player.getInventory().setItem(StarLobby.getInstance().getConfig().getInt("warps.slot"), warps);
+        player.getInventory().setItem(StarLobby.getInstance().getConfig().getInt("infos.slot"), infobook);
     }
 
     @EventHandler
