f3e3346859bb40fb8dd82e029c64bbcbf3ca05c6
Michael Mawick <michaeljens09@gmail.com>
2025-05-06 15:29:30 +0200

    Fixed bugs and added Loading System

--- src/main/java/de/michaelmawick/starlobby/StarLobby.java
+++ src/main/java/de/michaelmawick/starlobby/StarLobby.java
@@ -1,4 +1,41 @@
 package de.michaelmawick.starlobby;
 
-public class StarLobby {
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StarLobby extends JavaPlugin {
+
+    private StarLobby instance;
+
+    private final FileConfiguration config = getConfig();
+
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+        String msg = replacePlaceholders(config.getString("plugin-load-message"));
+        getServer().getConsoleSender().sendMessage(msg);
+    }
+
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        String msg = replacePlaceholders(config.getString("plugin-enable-message"));
+        getServer().getConsoleSender().sendMessage(msg);
+    }
+
+    @Override
+    public void onDisable() {
+        String msg = replacePlaceholders(config.getString("plugin-disable-message"));
+        getServer().getConsoleSender().sendMessage(msg);
+    }
+
+    public StarLobby getInstance() {
+        return instance;
+    }
+
+    public String replacePlaceholders(String input) {
+        return input.replace("%starlobby_prefix%", config.getString("prefix"));
+    }
+
 }
